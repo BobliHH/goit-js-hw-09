8ff063665a02464138f61194879c25b5ba5a5edc
=Andrei <balasca.andrei@gmail.com>
2023-05-17 21:30:17 +0300

    finished ex 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,12 +3,23 @@ const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 let timer = null;
 
+startBtn.addEventListener('click', onBtnStart);
+stopBtn.addEventListener('click', onBtnStop);
+
 function onBtnStart() {
-  startBtn.disabled = true;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
 
   timer = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+}
+
+function onBtnStop() {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    
+    clearInterval(timer);
 }
 
 function getRandomHexColor() {
