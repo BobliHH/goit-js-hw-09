4059295aa8a91c0f3b4116cddb9680e646456c59
=Andrei <balasca.andrei@gmail.com>
2023-05-20 16:57:10 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,47 @@
 import notiflix from 'notiflix';
+// notiflix.Notify.info('Cogito ergo sum');
+// notiflix.Notify.warning('Memento te hominem esse');
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector(['[name="amount"]']);
+
+form.addEventListener('click', onCreatePromises);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  delay.preventDefault();
+  new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return Promise;
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+  let newDelay = delay;
+  for (let i = 1; i <= amount; i++) {
+    newDelay += step;
+
+    createPromise(i, newDelay)
+      .then(({ position, delay }) => {
+        notiflix.Notify.success(
+          `✅ Fullfield promise ${position}in ${delay}ms`
+        );
+        // alert('success');
+      })
+      .catch(({ position, delay }) => {
+        notiflix.Notify.failure(`❌Rejected promise ${position}in ${delay}ms`);
+        // alert('fail');
+      });
   }
 }
