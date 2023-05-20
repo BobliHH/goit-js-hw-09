d89dc626b4298b2166cd4fa3bfa1142680ee24cd
=Andrei <balasca.andrei@gmail.com>
2023-05-20 19:36:23 +0300

    passed values from inputs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,11 +6,14 @@ const form = document.querySelector('.form');
 const delay = document.querySelector('[name="delay"]');
 const step = document.querySelector('[name="step"]');
 const amount = document.querySelector(['[name="amount"]']);
+console.log(form);
+console.log(delay);
+console.log(step);
+console.log(amount);
 
 form.addEventListener('click', onCreatePromises);
 
 function createPromise(position, delay) {
-  delay.preventDefault();
   new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
@@ -28,19 +31,26 @@ function createPromise(position, delay) {
 
 function onCreatePromises(e) {
   e.preventDefault();
-  let newDelay = delay;
-  for (let i = 1; i <= amount; i++) {
-    newDelay += step;
+  let newDelay = delay.value;
+  let newStep = step.value;
+  let newAmount = amount.value;
+
+  console.log(newDelay);
+  console.log(newStep);
+  console.log(newAmount);
+
+  for (let i = 1; i <= newAmount; i++) {
+    newDelay += newStep;
 
     createPromise(i, newDelay)
-      .then(({ position, delay }) => {
+      .then(({ position, newDelay }) => {
         notiflix.Notify.success(
-          `✅ Fullfield promise ${position}in ${delay}ms`
+          `✅ Fullfield promise ${position}in ${newDelay}ms`
         );
         // alert('success');
       })
-      .catch(({ position, delay }) => {
-        notiflix.Notify.failure(`❌Rejected promise ${position}in ${delay}ms`);
+      .catch(({ position, newDelay }) => {
+        notiflix.Notify.failure(`❌Rejected promise ${position}in ${newDelay}ms`);
         // alert('fail');
       });
   }
